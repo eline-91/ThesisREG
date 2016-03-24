3a85a71e860cfcbe4ae24d88fc6fe7104878ee10
Eline-91 <elinevanelburg@hotmail.com>
2016-03-24 17:42:23 +0100

    R code for rasClass package. Still in the beginning.

--- R/rasClassification.R
+++ R/rasClassification.R
@@ -7,17 +7,18 @@ library(rgdal)
 makeSampleRaster <- function(samplePolygons, rasterMosaic, cropRaster, filename, outputAsRaster = TRUE) {
   # Rasterize polygons
   classes <- rasterize(samplePolygons, rasterMosaic, field='Code')
-  print(class(rasterMosaic))
-  extent(rasterMosaic)
   # crop rasterized layer
   if (outputAsRaster == TRUE) {
-    croppedClasses <- crop(classes, cropRaster, filename = filename)
+    croppedClasses <- crop(classes, cropRaster, filename = filename, overwrite = TRUE)
     return(croppedClasses)
   } else {
-    crop(classes, cropRaster, filename = filename)
+    crop(classes, cropRaster, filename = filename, overwrite = TRUE)
     return(NULL)
   }
 }
+
+
+
 
 # Rasterize polygons for path 167 en path 168 and convert them to .asc to put in the asc files.
 sam <- readOGR('data/test', layer = 'samples_Merge')
@@ -26,8 +27,16 @@ sam@data$Code <- as.numeric(sam@data$Class)
 mosaicRas <- brick('data/tiff/path_167_168.tif')[[4]]
 listRasters <- list(brick('data/tiff/path_167.tif'), brick('data/tiff/path_168.tif'))
 fnList <- list('data/bricks/sampleRaster_167_1003', 'data/bricks/sampleRaster_168_1003')
+ascList <- list('data/ascFiles/path_167/path_167_sample.asc', 'data/ascFiles/path_168/path_168_sample.asc')
 
 for (i in 1:length(listRasters)) {
-  makeSampleRaster(sam, mosaicRas, listRasters[[i]], fnList[[i]], outputAsRaster = FALSE)
+  r <- makeSampleRaster(sam, mosaicRas, listRasters[[i]], fnList[[i]], outputAsRaster = TRUE)
+  raster::writeRaster(r, ascList[[i]], format = 'ascii', overwrite = TRUE)
 }
+
+
+
+
+
+
 
